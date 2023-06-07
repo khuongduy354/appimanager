fadd959504c082788201048c1123d2e731c4cb28
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-06-07 16:02:41 +0700

    feat(add): exec files, manually tested

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::File, io::Write, path::PathBuf};
+use std::{env, fs::File, io::Write, os::unix::prelude::PermissionsExt, path::PathBuf};
 
 use clap::{Parser, Subcommand};
 
@@ -68,7 +68,15 @@ fn get_abs_path(path: &PathBuf) -> PathBuf {
         path.clone()
     }
 }
-
+fn is_exec(path: &PathBuf) -> bool {
+    let metadata = std::fs::metadata(path).expect("cant get metadata");
+    let permissions = metadata.permissions();
+    if permissions.mode() & 0o111 != 0 {
+        return true;
+    } else {
+        return false;
+    }
+}
 fn main() -> Result<(), std::io::Error> {
     let app = Cli::parse();
     match app.commands {
@@ -80,40 +88,45 @@ fn main() -> Result<(), std::io::Error> {
             let dest_dir = get_abs_path(&dest_dir);
             let app_file = appimage_path.file_name().expect("AppImage must be a file!");
 
-            if appimage_path.is_file() {
+            if appimage_path.is_file() && is_exec(&appimage_path) {
                 let mut exec_path = get_abs_path(&appimage_path);
 
-                if let Some(ex) = appimage_path.extension() {
-                    if ex == "AppImage" {
-                        // move (if needed) before create .desktop
-                        if let Some(move_dir) = move_dir {
-                            if move_dir.is_dir() {
-                                let move_file_path = move_dir.join(&app_file);
-                                std::fs::rename(&appimage_path, &move_file_path)
-                                    .expect("cant move file");
-                                exec_path = get_abs_path(&move_file_path);
-                            }
-                        }
-
-                        // create .desktop
-                        make_desktop_file(&dest_dir, &exec_path)?;
-                    } else {
-                        println!("Not an AppImage");
+                // move (if needed) before create .desktop
+                if let Some(move_dir) = move_dir {
+                    if move_dir.is_dir() {
+                        let move_file_path = move_dir.join(&app_file);
+                        std::fs::rename(&appimage_path, &move_file_path).expect("cant move file");
+                        exec_path = get_abs_path(&move_file_path);
                     }
-                } else {
-                    println!("File dont have extension");
                 }
+
+                // create .desktop
+                make_desktop_file(&dest_dir, &exec_path)?;
             } else {
-                print!("AppImage not found");
+                print!("File not found, or not an executable!");
             }
         }
     }
     Ok(())
 }
 
+//TODO: after seperate in tests folder
+#[test]
+fn test_is_exec() {
+    // let test_file = PathBuf::from("./testasdf.AppImage");
+    // File::create(&test_file).unwrap();
+    //
+    // assert!(is_exec(&test_file));
+    //
+    // assert!(std::fs::remove_file("./testasdf.AppImage").is_ok());
+}
 //TODO: integration test, seperate test
 #[test]
 fn test_make_desktop_file() {
+    //create test file
+    let test_file = PathBuf::from("./testasdf.AppImage");
+    File::create(&test_file).unwrap();
+
     //relative dir test
     let dest_dir = get_abs_path(&PathBuf::from("."));
     let app_path = get_abs_path(&PathBuf::from("./testasdf.AppImage"));
@@ -127,6 +140,7 @@ fn test_make_desktop_file() {
     let desktop_file_path = dest_dir.join(app_file);
     assert!(desktop_file_path.is_file());
 
-    //remove that file
+    //cleanup testfiles
     assert!(std::fs::remove_file(desktop_file_path).is_ok());
+    assert!(std::fs::remove_file("./testasdf.AppImage").is_ok());
 }
